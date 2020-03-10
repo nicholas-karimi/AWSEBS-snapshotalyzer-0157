32c7c5b4608c7896e3baa01647414ef564126940
karimi254 <kariminic@gmail.com>
2020-03-10 13:29:58 +0300

    Added setup.py to enable building package.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='snapshotalyzer-0157',
+    version='0.1',
+    author='Nicholas Karimi',
+    author_email='nicholaskarimi.dev@gmail.com',
+    description='SanpshotAlyzer 0157 is a tool that manages AWS EC2 snapshots',
+    license='GPLV3+',
+    packages=['snappy'],
+    url='https://github.com/Karimi254/AWSEBS-snapshotalyzer-0157',
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_point='''
+    [console_scripts]
+    snappy=snappy.snappy:cli
+        ''',
+    )
