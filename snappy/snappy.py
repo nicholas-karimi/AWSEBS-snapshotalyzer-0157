672ef71ede06c9bb246b3d03876064cf950248f3
karimi254 <kariminic@gmail.com>
2020-03-10 03:03:54 +0300

    Modified the script to use the click package to provide more ec2 instance details

--- snappy/snappy.py
+++ snappy/snappy.py
@@ -1,8 +1,21 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='snappy')
+ec2 = session.resource('ec2')
+
+@click.command()
+def list_instances():
+    "List EC2 instances"
+    for instance in ec2.instances.all():
+        print(','.join((
+            instance.id,
+            instance.instance_type,
+            instance.placement['AvailabilityZone'],
+            instance.state['Name'],
+            instance.public_dns_name
+        )))
+    return
 
 if __name__ == '__main__':
-    session = boto3.Session(profile_name='snappy')
-    ec2 = session.resource('ec2')
-
-    for instance in ec2.instances.all():
-        print(instance)
+    list_instances()
